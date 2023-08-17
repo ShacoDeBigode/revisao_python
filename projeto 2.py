c439f2d21fb831dad579a1e15164c7a442781e48
PEDRO SIAS FERREIRA <pedro.ferreira9@granbery.edu.dti>
2023-08-16 21:59:46 -0300

    Atualizando

--- projeto 2.py
+++ projeto 2.py
@@ -1,4 +1,5 @@
-dic = {"nome" : "Pedro", "Idade" : "25", "Telefone" : "40028922"}
+dic = {"nome" : "Pedro", "Idade" : "25", "Telefone" : "40028922"}, {"Nome" : "Joao", "Idade" : "33", "Telefone" : "84meiamole60ficadura"}
+
 
 print(dic)
 
